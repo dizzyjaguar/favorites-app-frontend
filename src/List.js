c9529cd3bbc6ff9b1ff4d6455abeeba30010f009
Eli Zevin <dizzyjaguar@gmail.com>
2020-02-28 09:44:12 -0800

    create list component

--- src/List.js
+++ src/List.js
@@ -4,7 +4,9 @@ export default class List extends Component {
     render() {
         return (
             <div>
-                
+                {
+                    this.props.characters.map(char => <div className='character-card'> {char.name} </div>)
+                }
             </div>
         )
     }
